4e5f7cb5842aae384478bcd65eeea1f567c7a432
albasuades17 <asuademo8@alumnes.ub.edu>
2024-10-23 18:20:42 +0200

    Added styles to login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,64 +1,54 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const LoginPage: React.FC = () => {
   const router = useRouter();
-  const [isLogin, setIsLogin] = useState(true);
   const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   // Fakes a login 
   const handleLogin = () => {
-      localStorage.setItem('isAuthenticated', 'true');
-      router.push('/');
+    localStorage.setItem('isAuthenticated', 'true');
+    router.push('/');
   };
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-        <h2 style={{ margin: 10 }}>{isLogin ? 'Login' : 'Register'}</h2>
+    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', backgroundColor: '#fafaf5', height: '100vh'}}>
+      <div style={{ boxShadow: "0 1px 1px 0 grey", margin: 20, padding: 25, backgroundColor: 'white', display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center'}}>
+        <h2 style={{ margin: '10px 0px' }}>Login</h2>
         <form>
-            <div style={{ margin: 5}}>
-                <label> Username: </label><br />
-                <input
-                  type="text"
-                  id="username"
-                  value={username}
-                  onChange={setUsername}
-                  required
-                /><br />
-            </div>
-            {!isLogin && (
-                <div style={{ margin: 5}}>
-                    <label> Email: </label><br />
-                    <input
-                      type="text"
-                      id="email"
-                      value={email}
-                      onChange={setEmail}
-                      required
-                    /><br />
-                </div>
-            )}
-            <div style={{ margin: 5}}>
-                <label> Password: </label><br />
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={setPassword}
-                  required
-                /><br />
-            </div>
-            <button type="submit" onClick={handleLogin}>Login</button><br />
+          <div style={{ margin: 5}}>
+            <label> Username or email:</label><br />
+            <input type="text" id="username" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} required
+              style={{padding: 3}}
+            /><br />
+          </div>
+          <div style={{ margin: 5}}>
+            <label> Password: </label><br />
+            <input type="password" id="password"  
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} required
+              style={{padding: 3}}
+            /><br />
+          </div>
+          <div style={{ margin: '10px 5px'}}>
+            <button type="submit" onClick={handleLogin} style={{ width: '100%'}}>Login</button><br />
+          </div>
         </form>
-        <div>
-          <button style={{ color: 'blue', backgroundColor: 'white', padding: 0, margin: 10}}
-            onClick={() => setIsLogin(!isLogin)}
-          >
-            <u>{isLogin ? "Don't have an account? Register" : "Already have an account? Login"}</u>
+        <div style={{ margin: 5}}>
+          <button style={{ color: '#417154', backgroundColor: 'white', padding: 0, width: '100%'}} className="linkButton">
+            <u>Don't have an account? Register</u>
           </button>
+          {/* Styled JSX block */}
+          <style jsx>{`
+            .linkButton:hover {
+              color: #417154d5;
+            }
+          `}</style>
         </div>
+      </div>
     </div>
   );
 };
