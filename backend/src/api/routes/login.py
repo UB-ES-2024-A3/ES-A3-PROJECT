a5819d79c9aa4c0b9e75bbcb14fa057a5fd7693e
lmestres <lmestres77@gmail.com>
2024-10-29 11:22:00 +0100

    endpoint for login + tests (missing files)

---
+++ backend/src/api/routes/login.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, HTTPException, Response
+from backend.src import crud
+
+router = APIRouter()
+from pydantic import BaseModel
+
+class AuthRequest(BaseModel):
+    credentials: str
+    password: str
+# Endpoint to authenticate a user by username or email
+@router.post("/login")
+async def authenticate(authRequest: AuthRequest):
+    try:
+        auth = crud.user.authenticate(authRequest.credentials, authRequest.password)
+        if auth:
+            return Response(status_code=204)
+        else:
+            raise HTTPException(status_code=500, detail={"credentials": "Incorrect username/email or password"})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ backend/src/tests/api/routes/test_routes_login.py
@@ -0,0 +1,54 @@
+from backend.src.main import app
+from backend.src import crud
+
+
+import pytest
+from fastapi.testclient import TestClient
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+def test_authenticate_correct_user_email(client: TestClient):
+    users = crud.user.read_users()
+    data = {"credentials" : users[0].email, "password": users[0].password}
+    r = client.post(f"/login", json = data)
+    assert r.status_code == 204, f"Expected 204, got {r.status_code}. Details: {r.json()}"
+
+
+def test_authenticate_correct_user_username(client: TestClient):
+    users = crud.user.read_users()
+    data = {"credentials": users[0].username, "password": users[0].password}
+    r = client.post(f"/login", json = data)
+    assert r.status_code == 204, f"Expected 204, got {r.status_code}. Details: {r.json()}"
+
+def test_authenticate_wrong_user(client: TestClient):
+    data = {"credentials": "wrong@gmail.com", "password": "wrongPassword"}
+    try:
+        r = client.post(f"/login", json = data)
+    except Exception as e:
+        assert e.detail == "Incorrect username/email or password"
+
+def test_authenticate_wrong_user_email(client: TestClient):
+    users = crud.user.read_users()
+    data = {"credentials": "wrong@gmail.com", "password": users[0].password}
+    try:
+        r = client.post(f"/login", json = data)
+    except Exception as e:
+        assert e.detail == "Incorrect username/email or password"
+
+def test_authenticate_wrong_user_username(client: TestClient):
+    users = crud.user.read_users()
+    data = {"credentials": "wrongUsername", "password": users[0].password}
+    try:
+        r = client.post(f"/login", json = data)
+    except Exception as e:
+        assert e.detail == "Incorrect username/email or password"
+
+def test_authenticate_wrong_user_password(client: TestClient):
+    users = crud.user.read_users()
+    data = {"credentials": users[0].username, "password": "wrongPassword"}
+    try:
+        r = client.post(f"/login", json = data)
+    except Exception as e:
+        assert e.detail == "Incorrect username/email or password"
