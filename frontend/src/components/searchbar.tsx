39a6f1b6aa0bbe2cdae1974e67c313e8de76f461
albasuades17 <albasuadesm@gmail.com>
2024-11-11 16:14:04 +0100

    Added the component for displaying information while searching

--- frontend/src/components/searchbar.tsx
+++ frontend/src/components/searchbar.tsx
@@ -1,47 +1,57 @@
 import React, { useState } from 'react';
+import BookBar from './bookbar';
 
 interface SearchBarProps {
   placeholder?: string;
   buttonLabel?: string;
   onSearch: (query: string) => void;
+  searchResults: {id: string; title: string; author: string}[];
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ placeholder, buttonLabel, onSearch }) => {
+const SearchBar: React.FC<SearchBarProps> = ({ placeholder, buttonLabel, onSearch, searchResults }) => {
   const [query, setQuery] = useState('');
 
   const handleSearch = () => {
     onSearch(query); 
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    onSearch(newQuery); // Update search results while typing
+  };
+
   return (
-    <div style={{ display: 'flex'}}>
-      <input
-        type="text"
-        placeholder={placeholder}
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        style={{
-          padding: '10px',
-          fontSize: '16px',
-          border: '1px solid #ccc',
-          borderRadius: '5px 0 0 5px',
-          width: '250px',
-        }}
-      />
-      <button
-        onClick={handleSearch}
-        style={{
-          padding: '10px 20px',
-          fontSize: '16px',
-          backgroundColor: '#417154',
-          color: 'white',
-          border: 'none',
-          borderRadius: '0 5px 5px 0',
-          cursor: 'pointer',
-        }}
-      >
-        {buttonLabel}
-      </button>
+    <div>
+      <div style={{ display: 'flex'}}>
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={query}
+          onChange={handleInputChange}
+          style={{
+            padding: '10px',
+            fontSize: '16px',
+            border: '1px solid #ccc',
+            borderRadius: '5px 0 0 5px',
+            width: '250px',
+            margin: '0px'
+          }}
+        />
+        <button
+          onClick={handleSearch}
+          style={{
+            borderRadius: '0 5px 5px 0'
+          }}
+        >
+          {buttonLabel}
+        </button>
+      </div>
+      <div style={{ marginTop: '0px', maxHeight: '200px', overflowY: 'auto', display: 'flex', flexDirection: 'column', position: 'absolute'}}>
+        {searchResults.map((book) => (
+          <BookBar key={book.id} id={book.id} title={book.title} author={book.author} />
+        ))}
+      </div>
     </div>
   );
 };
