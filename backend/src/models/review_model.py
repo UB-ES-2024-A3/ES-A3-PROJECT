30ca7231666f828c32573c951c6117dc5ed5d1e8
lmestres <lmestres77@gmail.com>
2024-11-15 10:22:20 +0100

    changed variable name

--- backend/src/models/review_model.py
+++ backend/src/models/review_model.py
@@ -5,14 +5,14 @@ from datetime import date, time
 class Review(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     _comment: str
-    _rating: int
+    _stars: int
     _date: date
     _time: time
     _book_id: str
     _user_id: str
 
     def __repr__(self):
-        return f"Review(id='{self.id}, comment='{self._comment}, date='{self._date}, time='{self._time},book_id='{self._book_id}, user_id='{self._user_id}')"
+        return f"Review(id='{self.id}, comment='{self._comment}, stars='{self._stars}, date='{self._date}, time='{self._time},book_id='{self._book_id}, user_id='{self._user_id}')"
     
     class Config:
         from_attributes = True
@@ -40,7 +40,7 @@ class Review(BaseModel):
         return self._user_id
     
     @property
-    def rating(self) -> int:
-        return self._rating
+    def stars(self) -> int:
+        return self._stars
     
     # None of these field should be modified so no setter is implemented
