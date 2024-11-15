5f72a7dfb983b65d751c5f6cca6cd3a8ea5b4492
lmestres <lmestres77@gmail.com>
2024-11-15 16:40:41 +0100

    return fix

--- backend/src/tests/api/routes/test_routes_users.py
+++ backend/src/tests/api/routes/test_routes_users.py
@@ -17,11 +17,10 @@ def test_get_username_with_valid_id(client: TestClient):
     created_user = crud.user.create_user(user)
     response = client.get(f"/users/username/id/{created_user.id}")
     assert response.status_code == 200, f"Expected 200, got {response.status_code}. Details: {response.json()}"  
-    assert response.json() == created_user.id  
+    assert response.json() == created_user.username  
     crud.user.delete_user(created_user.id)
 
 def test_get_username_with_invalid_id(client: TestClient):
     id = str(uuid.uuid4())
     response = client.get(f"/users/username/id/{id}")
     assert response.status_code == 404, f"Expected 404, got {response.status_code}. Details: {response.json()}"  
-    crud.user.delete_user(id)
