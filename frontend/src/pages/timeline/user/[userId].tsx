04773367a9743f63603dffc54c645b02a51f5b7b
fanusky <fmariaar11@alumnes.ub.edu>
2024-11-27 00:27:38 +0100

    Added follow button, followers and following to users profile.

--- frontend/src/pages/timeline/user/[userId].tsx
+++ frontend/src/pages/timeline/user/[userId].tsx
@@ -15,26 +15,24 @@ export interface UserReviewCardProps {
     book_id: string
   }
 
-const Profile = () => {
-  const router = useRouter();
+const UserProfile = () => {
   const [reviews, setReviews] = useState<UserReviewCardProps[]>([]);
-  const [userId, setUserId] = useState<string | null>(null);
+  const router = useRouter();
+  const { userId } = router.query;
   const [username, setUsername] = useState('');
+  const [follows, setFollows] = useState(false);
+  const [followButton, setFollowButton] = useState({label: "Follow", style: ""});
 
   useEffect(() => {
     const storedUserId = localStorage.getItem('userId');
-    setUserId(storedUserId); 
+    if (storedUserId == userId) {
+        router.push("/profile")
+    }
   }, []);
 
-  const handleLogout = () => {
-    localStorage.removeItem('isAuthenticated');
-    localStorage.removeItem('userId');
-    router.push('/login');
-  };
-
   useEffect(() => {
-    if (userId){
-        ReviewService.getUserReviews(userId)
+    if (userId && router.isReady){
+        ReviewService.getUserReviews(String(userId))
         .then(reviewList => {
             setReviews(reviewList);
         })
@@ -42,9 +40,8 @@ const Profile = () => {
             console.log(except);
             setReviews([]);
         });
-        UserService.getUsername(userId)
+        UserService.getUsername(String(userId))
         .then(userName => {
-            console.log(userName)
             setUsername(userName);
         })
         .catch(except => {
@@ -52,14 +49,39 @@ const Profile = () => {
             setUsername('');
         });
     }
-}, [userId]);
+}, [userId, router.isReady]);
+
+const handleFollow = () => {
+    setFollows(!follows);
+    if (follows) {
+        setFollowButton({label: "Follow", style: ""});
+    }
+    else{
+        setFollowButton({label: "Unfollow", style: "secondaryButton"})
+    }
+    
+}
   return (
     <NavBar>
         <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <div style={{ width: '65%', display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
                 <header style={{ padding: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderBottom: '1px solid #ccc' }}>
-                    <h1 style={{ fontSize: '1.2rem', fontWeight: '500' }}>{username}</h1>
-                    <button onClick={handleLogout}>Logout</button>
+                    <div>
+                        <h1 style={{ fontSize: '1.2rem', fontWeight: '500' }}>{username}</h1>
+                        <div
+                            style={{
+                            display: 'flex',
+                            marginTop: '8px',
+                            gap: '10px'
+                            }}
+                        >
+                            <span style={{ fontWeight: 'bold', fontSize: '0.85rem' }}>100</span>
+                            <span style={{ fontSize: '0.85rem', color: '#6b7280' }}>Followers</span>
+                            <span style={{ fontWeight: 'bold', fontSize: '0.85rem', marginLeft: '10px' }}>100</span>
+                            <span style={{ fontSize: '0.85rem', color: '#6b7280' }}>Following</span>
+                        </div>
+                    </div>
+                    <button onClick={handleFollow} className={followButton.style}>{followButton.label}</button>
                 </header>
                 <ProfileContents reviews={reviews}/>
             </div>
@@ -68,4 +90,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default UserProfile;
