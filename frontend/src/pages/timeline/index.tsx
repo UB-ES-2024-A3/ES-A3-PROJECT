d302298b5b49897342b57d420fd78096a4ba5c33
albasuades17 <albasuadesm@gmail.com>
2024-11-26 09:24:31 +0100

    Change the structure of the frontend to make list of searched books (and users) a page, instead of a component

--- frontend/src/pages/timeline/index.tsx
+++ frontend/src/pages/timeline/index.tsx
@@ -26,16 +26,10 @@ const Timeline = () => {
     return (
     <NavBar>
         <SearchBar placeholder="Search..." buttonLabel="Search">
-            {search ? (
-            <div style={{margin: '5px', width: '100%'}}> 
-                <ListSearch search={search} />
-            </div>
-        ) : (
             <div style={{margin: '5px', textAlign: 'center', justifyContent: 'center', height: '80vh', display: 'flex', flexDirection: 'column'}}> 
                 <h2 style={{fontSize: '2em', color: 'grey'}}>Find a book!</h2>
                 <h3 style={{color: 'grey'}}>Search you favourite book to add a review.</h3>
             </div>
-        )}
         </SearchBar>
     </NavBar>
     );
