4f4a410a5efcf357b1f5775269eeb6ffc32213d1
DavMol2001 <dmolinba24@alumnes.ub.edu>
2024-11-27 15:58:33 +0100

    First test done

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: 'ts-jest', // Use ts-jest for TypeScript
+    testEnvironment: 'jest-environment-jsdom', // Use jsdom for testing React components
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Set up testing-library
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest', // Use ts-jest for .ts and .tsx files
+    },
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock CSS modules
+    },
+    transformIgnorePatterns: ['<rootDir>/node_modules/'], // Avoid transforming node_modules
+  };
+  
