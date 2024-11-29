4eb94e2607bc1f9bc2acfe61b6525d27bdcde8c5
BlaiG <blgabalp7@alumnes.ub.edu>
2024-11-29 10:20:20 +0100

    Created review card component for the timeline

---
+++ frontend/src/components/timeline_review_card.tsx
@@ -0,0 +1,51 @@
+import { renderStars } from "./stars_rating"
+
+export interface TimelineReviewProps {
+    userId: string,
+    username: string,
+    rating: number,
+    bookId: string,
+    bookTitle: string,
+    author: string,
+    comment?: string,
+    date?: string,
+    time?: string
+}
+
+export default function TimelineReviewCard({ userId, username, bookId, bookTitle, author, rating, comment, date, time }: TimelineReviewProps) {
+    return (
+        <div style={{ height: "fit-content", minWidth: "100%", margin: "0 auto", padding: "24px", borderRadius: "8px", backgroundColor: "white"}}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <div style={{ flexGrow: 1 }}>
+                    <button className="secondaryButton" style={{ fontWeight: "bold" }}>
+                        {username}
+                    </button>
+                </div>
+                {date && time && (
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <span style={{ fontWeight: "bold" }}>{date}</span>
+                        <span style={{ marginLeft: "8px" }}>{time}</span>
+                    </div>
+                )}
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", borderTop: "solid black 1px", padding: "16px"}}>
+                <div>
+                    <button className="secondaryButton" style={{ fontSize: "1.25rem", fontWeight: "bold", display: "contents" }} >
+                        {bookTitle}
+                    </button>
+                    <p style={{ color: "grey", marginTop: "4px" }}>{author}</p>
+                </div>
+                <div style={{ display: "flex", marginTop: "8px", alignItems: "flex-end" }}>
+                    {renderStars(rating)} 
+                </div>
+
+            </div>
+            {comment && (
+                <p style={{ lineHeight: "1.625", marginTop: "16px" }}>
+                    {comment}
+                </p>
+            )}
+            
+        </div>
+    );
+}
--- frontend/src/pages/timeline/index.tsx
+++ frontend/src/pages/timeline/index.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/components/searchbar';
 import ListBooks from '@/components//list_books';
 import { useTimelineContext } from '@/contexts/TimelineContext';
 import NavBar from '@/components/navbar';
+import TimelineReviewCard, { TimelineReviewProps } from '@/components/timeline_review_card';
 
 export interface Book{
     id: string,
@@ -12,7 +13,21 @@ export interface Book{
     genres: string[],
     avgstars: number,
     numreviews: number
-  }
+}
+
+const reviews: TimelineReviewProps[] = [
+    {
+        userId: '0001',
+        username: 'bookworm',
+        bookId: '1000',
+        bookTitle: 'The Hunger Games',
+        author: 'Suzanne Collins',
+        rating: 5,
+        comment: 'What an increadible story!',
+        date: '12/05/2024',
+        time: '14:32:07'
+    }
+];
 
 const Timeline = () => {
     const {timelineState} = useTimelineContext();
@@ -31,10 +46,21 @@ const Timeline = () => {
                 <ListBooks search={search} />
             </div>
         ) : (
-            <div style={{margin: '5px', textAlign: 'center', justifyContent: 'center', height: '80vh', display: 'flex', flexDirection: 'column'}}> 
-                <h2 style={{fontSize: '2em', color: 'grey'}}>Find a book!</h2>
-                <h3 style={{color: 'grey'}}>Search you favourite book to add a review.</h3>
-            </div>
+            <TimelineReviewCard
+                userId={reviews[0].userId}
+                username={reviews[0].username}
+                bookId={reviews[0].bookId}
+                bookTitle={reviews[0].bookTitle}
+                author={reviews[0].author}
+                rating={reviews[0].rating}
+                comment={reviews[0].comment}
+                date={reviews[0].date}
+                time={reviews[0].time}
+            />
+            // <div style={{margin: '5px', textAlign: 'center', justifyContent: 'center', height: '80vh', display: 'flex', flexDirection: 'column'}}> 
+            //     <h2 style={{fontSize: '2em', color: 'grey'}}>Find a book!</h2>
+            //     <h3 style={{color: 'grey'}}>Search you favourite book to add a review.</h3>
+            // </div>
         )}
         </SearchBar>
     </NavBar>
