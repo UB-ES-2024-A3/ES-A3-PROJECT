882a15a19646a368a23f128289c4e8225b30a4b7
BlaiG <blgabalp7@alumnes.ub.edu>
2024-11-29 16:35:18 +0100

    Display list of reviews in timeline

--- frontend/src/pages/timeline/index.tsx
+++ frontend/src/pages/timeline/index.tsx
@@ -25,6 +25,16 @@ const reviews: TimelineReviewProps[] = [
         comment: 'What an increadible story!',
         date: '12/05/2024',
         time: '14:32:07'
+    },
+    {
+        userId: '0002',
+        username: 'avid-reader',
+        bookId: '8468d0fe-5e08-4594-a86d-92a239e8ad6e',
+        bookTitle: 'The Nativity Story',
+        author: 'Voiland, Stephanie (EDT)',
+        rating: 4,
+        date: '10/05/2024',
+        time: '18:13:22'
     }
 ];
 
@@ -40,21 +50,28 @@ const Timeline = () => {
     return (
     <NavBar>
         <SearchBar placeholder="Search..." buttonLabel="Search" id='searchbar'>
-            <TimelineReviewCard
-                userId={reviews[0].userId}
-                username={reviews[0].username}
-                bookId={reviews[0].bookId}
-                bookTitle={reviews[0].bookTitle}
-                author={reviews[0].author}
-                rating={reviews[0].rating}
-                comment={reviews[0].comment}
-                date={reviews[0].date}
-                time={reviews[0].time}
-            />
-            // <div style={{margin: '5px', textAlign: 'center', justifyContent: 'center', height: '80vh', display: 'flex', flexDirection: 'column'}}> 
-            //     <h2 style={{fontSize: '2em', color: 'grey'}}>Find a book!</h2>
-            //     <h3 style={{color: 'grey'}}>Search you favourite book to add a review.</h3>
-            // </div>
+            <main style={{ display: 'flex', flexDirection: 'column', gap: '16px', padding: '16px', width: '70%' }}>
+                {reviews.length? (reviews.map((review) => {
+                    return (
+                        <TimelineReviewCard
+                            userId={review.userId}
+                            username={review.username}
+                            bookId={review.bookId}
+                            bookTitle={review.bookTitle}
+                            author={review.author}
+                            rating={review.rating}
+                            comment={review.comment}
+                            date={review.date}
+                            time={review.time}
+                        />
+                    );
+                })) : (
+                    <div style={{margin: '5px', textAlign: 'center', justifyContent: 'center', height: '80vh', display: 'flex', flexDirection: 'column'}}> 
+                        <h2 style={{fontSize: '2em', color: 'grey'}}>No recent reviews found</h2>
+                        <h3 style={{color: 'grey'}}>Search your favourite book to add a review</h3>
+                    </div>
+                )}
+            </main>
         </SearchBar>
     </NavBar>
     );
