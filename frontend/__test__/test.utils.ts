fb3c0f8dc15efdf56407f8efeafba678c91e38af
fanusky <fmariaar11@alumnes.ub.edu>
2024-12-01 14:29:35 +0100

    Created E2E tests for the register page.

--- frontend/__test__/test.utils.ts
+++ frontend/__test__/test.utils.ts
@@ -6,7 +6,6 @@ import chrome from 'selenium-webdriver/chrome';
 export const createWebDriver = async (): Promise<WebDriver> => {
   const options = new chrome.Options();
   options.addArguments(
-    '--headless',
     '--no-sandbox',
     '--disable-dev-shm-usage',
     '--disable-gpu',
