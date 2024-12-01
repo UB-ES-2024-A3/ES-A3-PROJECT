fe5b58f8c423f183d2945f73b25355864788ed7b
fanusky <fmariaar11@alumnes.ub.edu>
2024-12-01 21:55:33 +0100

    Changed initial redirection implementation.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,20 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { TimelineProvider } from '../contexts/TimelineContext';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const authStatus = localStorage.getItem('isAuthenticated');
-    if (authStatus === 'true') {
-      router.push('/profile');
-    } else {
-      router.push('/login');
-    }
-  }, []);
   
   return (
     <TimelineProvider>
