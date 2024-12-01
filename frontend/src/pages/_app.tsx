0cb978a0fc781e36ea364befe064af5f3b7c8b63
fanusky <fmariaar11@alumnes.ub.edu>
2024-12-01 14:29:35 +0100

    Created E2E tests for the register page.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,21 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { TimelineProvider } from '../contexts/TimelineContext';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const authStatus = localStorage.getItem('isAuthenticated');
-    if (authStatus === 'true') {
-      router.push('/profile');
-    } else {
-      router.push('/login');
-    }
-  }, []);
   
   return (
     <>
