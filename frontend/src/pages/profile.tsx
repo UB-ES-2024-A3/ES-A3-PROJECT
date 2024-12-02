01fd4b24d1b5b2033443c4e831587dcad3d5abd3
fanusky <fmariaar11@alumnes.ub.edu>
2024-12-02 17:27:57 +0100

    Fixed rebase with development.

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -11,18 +11,15 @@ const Profile = () => {
   const [reviews, setReviews] = useState<UserReviewCardProps[]>([]);
   const [userId, setUserId] = useState<string | null>(null);
   const [username, setUsername] = useState('');
-
   useEffect(() => {
     const storedUserId = localStorage.getItem('userId');
     setUserId(storedUserId); 
   }, []);
-
   const handleLogout = () => {
     localStorage.removeItem('isAuthenticated');
     localStorage.removeItem('userId');
     router.push('/login');
   };
-
   useEffect(() => {
     if (userId){
         ReviewService.getUserReviews(userId)
@@ -63,38 +60,12 @@ const Profile = () => {
                             <span style={{ fontSize: '0.85rem', color: '#6b7280' }}>Following</span>
                         </div>
                     </div>                    
-                    <button onClick={handleLogout}>Logout</button>
+                    <button id="logout_button" onClick={handleLogout}>Logout</button>
                 </header>
-                <div style={{ width: '100%', borderTop: '1px solid #bbb' }}>
-                    <ProfileNavBar activeTab={activeTab} setActiveTab={setActiveTab} />
-                </div>
-                <main style={{ flex: 1, padding: '16px' }}>
-                {activeTab === 'reviews' ? (
-                    <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: '16px',
-                        overflowY: 'visible',
-                        paddingRight: '16px',
-                    }}
-                    >
-                    {reviews.length? (reviews.map((review, index) => (
-                        <ProfileReviewCard key={index} bookTitle={review.title} author={review.author} rating={review.stars} date={review.date} time={review.time} review={review.comment} book_id={review.book_id}/>
-                    ))):(
-                        <div style={{margin: '5px', textAlign: 'center', justifyContent: 'center', height: '80vh', display: 'flex', flexDirection: 'column'}}> 
-                            <h2 style={{fontSize: '2em', color: 'grey'}}>You have no reviews yet.</h2>
-                        </div>
-                    )}
-                    </div>
-                ) : (
-                    <p>Loading...</p>
-                )}
-                </main>
+                <ProfileContents reviews={reviews}/>
             </div>
         </div>
     </NavBar>
-  );
+    );
 };
-
 export default Profile;
